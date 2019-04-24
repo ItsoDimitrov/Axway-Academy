44e248a77e7f1f79226057f4c9dd762dd13e0842
ItsoDimitrov <fffsssccc@abv.bg>
2019-04-24 21:21:42 +0300

    Race condition code added

--- 23.04.19 - Asynchronous Programming/02.RaceCondition/RaceConditionExample.cs
+++ 23.04.19 - Asynchronous Programming/02.RaceCondition/RaceConditionExample.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace _02.RaceCondition
 {
-    class Program
+    public class RaceConditionExample
     {
+        private static List<int> _numbers;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _numbers = Enumerable.Range(0, 100).ToList();
+            //Thread t1 = new Thread(RemoveFromList);
+            //Thread t2 = new Thread(RemoveFromList);
+            //Thread t3 = new Thread(RemoveFromList);
+            List<Thread> threads = new List<Thread>
+            {
+                new Thread(RemoveFromList),
+                new Thread(RemoveFromList),
+                new Thread(RemoveFromList),
+
+            };
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+
         }
+
+        public static void RemoveFromList()
+        {
+            while (_numbers.Count > 0)
+            {
+                int lastIndex = _numbers.Count - 1;
+                _numbers.RemoveAt(lastIndex);
+            }
+        }
+
     }
 }
