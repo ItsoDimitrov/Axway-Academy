acb2a3127503e77471be246326c74985e88ed1d3
ItsoDimitrov <fffsssccc@abv.bg>
2019-04-24 19:21:19 +0300

    Asynchronous operation example with Tasks

---
+++ 23.04.19 - Asynchronous Programming/01.AsynchronousCodeExample/Asynchronous.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01.AsynchronousCodeExample
+{
+    class Asynchronous
+    {
+        static void Main(string[] args)
+        {
+
+            Task task = new Task(TakeMaxValueOfInt);
+            task.Start();
+            Console.WriteLine("Im calculating but you are not blocked");
+            string command = Console.ReadLine();
+            while (true)
+            {
+                if (command == "exit")
+                {
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+
+        private static void TakeMaxValueOfInt()
+        {
+            for (int i = 0; true; i++)
+            {
+                if (i == int.MaxValue)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
