d9c6aefcdb42d162673e424bb0c2c9377dcc8834
ItsoDimitrov <fffsssccc@abv.bg>
2019-05-08 17:29:13 +0300

    Initial Create - Folder for homework оn 07.05.19 - Database

---
+++ 07.05.19 - EF AdoNET Database/Axway/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Axway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
