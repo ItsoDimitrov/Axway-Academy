e2ffd046dcbd498a619d30c8d5772ed56508ea11
ItsoDimitrov <fffsssccc@abv.bg>
2019-05-09 23:12:03 +0300

    Example added in StartUp class for EF and Ado.Net

--- 07.05.19 - EF AdoNET Database/Axway/StartUp.cs
+++ 07.05.19 - EF AdoNET Database/Axway/StartUp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using Axway.Database;
 using Microsoft.EntityFrameworkCore.Internal;
@@ -11,8 +12,32 @@ namespace Axway
         static void Main(string[] args)
         {
             AxwayContext context = new AxwayContext();
-            
-           
+            //EF 
+            var student = context.Students.Select(s => s.Name).FirstOrDefault();
+            Console.WriteLine(student);
+
+            //ADO.NET
+            string queryString = @"SELECT TOP(1) [s].[Name]
+                                            FROM [Students] AS [s]";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionConfiguration.CONNECTION_STRING))
+            {
+                SqlCommand command = new SqlCommand(
+                    
+                    queryString,connection);
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader);
+                    }
+                }
+            }
+
+
+
+
         }
     }
 }
