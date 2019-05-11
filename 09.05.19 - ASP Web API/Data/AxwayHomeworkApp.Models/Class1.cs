95e8f186eb9047b34a793b31406166b5593a515e
ItsoDimitrov <fffsssccc@abv.bg>
2019-05-11 13:06:46 +0300

    Created class " Game " and properties

--- 09.05.19 - ASP Web API/Data/AxwayHomeworkApp.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AxwayHomeworkApp.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ 09.05.19 - ASP Web API/Data/AxwayHomeworkApp.Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AxwayHomeworkApp.Models.Enums;
+
+namespace AxwayHomeworkApp.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public Genre Genre { get; set; }
+
+    }
+}
