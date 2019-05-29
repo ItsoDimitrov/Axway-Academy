5428f9d4e2c116eb49707b91eb40005acc65561e
ItsoDimitrov <fffsssccc@abv.bg>
2019-05-29 21:03:09 +0300

    Created xunit project for tests

---
+++ 28.05.19 - Testing TDD/Tests/Homework.Tests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Homework.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ 28.05.19 - Testing TDD/Tests/Program.Tests/ProgramTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xunit;
+
+namespace Program.Tests
+{
+    public class ProgramTests
+    {
+        
+    }
+}
